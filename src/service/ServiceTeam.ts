b51cd49660eb42c9554e1a1a5ccc98684903477f
Nicolas Lima <nichollaslimma734@gmail.com>
2023-04-26 20:28:40 -0300

    criado services

---
+++ src/service/ServiceTeam.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+import db from '../config/db';
+
+
+class ServiceTeam{
+    public async find(req:Request, res:Response){
+        try{
+
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+
+    public async findById(req:Request, res:Response){
+        try{
+
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+
+    public async create(req:Request, res:Response){
+        try{
+
+        }catch(error){
+            res.status(500).json(error);
+        }
+    } 
+
+    public async update(req:Request, res:Response){
+        try{
+
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+
+    public async delete(req:Request, res:Response){
+        try{
+
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+}
+
+export default new ServiceTeam();
